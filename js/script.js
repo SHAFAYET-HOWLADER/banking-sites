056485dae403cf06691e0e2bb0d9c1d106789e6a
unknown <you@example.com>
2022-02-10 21:06:18 +0600

    commited

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@
 //submit event
 document.getElementById("submit").addEventListener("click", function(){
     const emailInput = document.getElementById("emailBox");
-    console.log(emailInput.value);
+    const userEmail = emailInput.value;
     const  passInput = document.getElementById("passBox");
-    console.log(passInput.value);
-   if((emailInput == "safayethowlader96@gmail.com") && (passInput == 00000)){
-       window.location.href = "banking.html";
+    const userPassword =     passInput.value;
+   if((userEmail == "safayethowlader96@gmail.com") && (userPassword =="s1a2f3a4")){
+      window.location.href = "banking.html";
    }
 })
+
